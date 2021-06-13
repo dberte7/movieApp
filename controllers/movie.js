973ec90aac5618695761214c06b1f98d26007a71
Alicia1226 <alicia.verdugo.payo@gmail.com>
2021-06-13 19:38:23 +0200

    update movies.pug & movies.js

--- controllers/movie.js
+++ controllers/movie.js
@@ -2,88 +2,72 @@ const Film = require('../models/Film');
 const movies = require('../utils/movies');
 const getMoviesToDB = require('../utils/getMoviesToDB');
 const apikey = process.env.API_KEY;
+//Variable user temporal
+let user = true;
 
 const routes = {
-    home:(req, res) => {
-        res.status(200).send('vista inicial/login')
+    signIn:(req, res) => {
+        res.status(200).render('movies', {signIn: true})
     },
-    // getSingUp: ,
-    // postSingUp: ,
-    // login:,
-    // logout:,
     dashboard:(req, res) => {
-        res.status(200).render('dashboard')
+        res.status(200).render('movies', {dashboard: true})
     },
     getMovies: async (req, res)=>{
         let titleQ = req.query.s
         let search = [];
-        Film.exists({ searchKeyword: titleQ.toLowerCase() }, async (err,result)=>{
-            if(err){
-                console.log(error);
-            } else {
-                if(result){
-                    try {
-                        const data = await Film.find({ searchKeyword:titleQ.toLowerCase() });
-                        console.log("base de datos");
-                        res.status(200).json(data); 
-                    } catch (err) {
-                        res.status(500).json({ message: err.message });
-                    }
+        if (titleQ === undefined) {
+            res.status(200).render('movies', {searchPage: true})
+        } else {
+            try {
+                let data = await movies.getfilm(`http://www.omdbapi.com/?s=${titleQ}&type=movie&apikey=${apikey}&`);
+                if (data.Response === false){
+                    res.status(500).json({ message: `${data.Error}`});
                 } else {
-                    try {
-                        let data = await movies.getfilm(`http://www.omdbapi.com/?s=${titleQ}&type=movie&apikey=${apikey}&`);
-                        if (data.Response === false){
-                            res.status(500).json({ message: `${data.Error}`});
-                        } else {
-                            for (let index = 0; index < data.Search.length; index++) {
-                                let id = data.Search[index].imdbID;
-                                let data2 = await movies.getfilm(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
-                                search.push(data2);
-                                getMoviesToDB.arrayToDB(data2, titleQ);
-                            }
-                            res.status(200).json(search);
-                        }
-                    } catch (err) {
-                        res.status(500).json({ message: err.message });
+                    for (let index = 0; index < data.Search.length; index++) {
+                        let id = data.Search[index].imdbID;
+                        let data2 = await movies.getfilm(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
+                        search.push(data2);
+                        //getMoviesToDB.arrayToDB(data2, titleQ);
                     }
                 }
-                
+            } catch (err) {
+                res.status(500).json({ message: err.message });
             }
-        })
-        
+            res.status(200).render('movies', {searchPage: true, search:search})
+        }    
     },
     searchTitle: async(req, res) => {
-        let titleQ = req.params.title
-        let search = [];
-        Film.exists({ searchKeyword: titleQ.toLowerCase() }, async (err,result)=>{
-            if(err){
-                console.log(error);
-            } else {
-                if(result){
-                    try {
-                        const data = await Film.find({ searchKeyword:titleQ.toLowerCase() });
-                        console.log("base de datos");
-                        res.status(200).json(data); 
-                    } catch (err) {
-                        res.status(500).json({ message: err.message });
-                    }
-                } else {
-                    try {
-                        let data = await movies.getfilm(`http://www.omdbapi.com/?s=${titleQ}&type=movie&apikey=${apikey}&`);
-                        for (let index = 0; index < data.Search.length; index++) {
-                            let id = data.Search[index].imdbID;
-                            let data2 = await movies.getfilm(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
-                            search.push(data2);
-                            getMoviesToDB.arrayToDB(data2, titleQ);
-                        }
-                        res.status(200).json(search);
-                    } catch (err) {
-                        res.status(500).json({ message: err.message });
-                    }
-                }
-                
+        
+let titleQ = req.params.title
+let search = [];
+Film.exists({ title: titleQ}, async (err,result)=>{
+    if(err){
+        console.log(error);
+    } else {
+        if(result){
+            try {
+                const data = await Film.find({ title:titleQ});
+                console.log("base de datos");
+                res.status(200).render('movies', {detail: true, data:data});
+            } catch (err) {
+                res.status(500).json({ message: err.message });
             }
-        })
+        } else {
+            try {
+                let data = await movies.getfilm(`http://www.omdbapi.com/?s=${titleQ}&type=movie&apikey=${apikey}&`);
+                for (let index = 0; index < data.Search.length; index++) {
+                    let id = data.Search[index].imdbID;
+                    let data2 = await movies.getfilm(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
+                    search.push(data2);
+                    getMoviesToDB.arrayToDB(data2, titleQ);
+                }
+                res.status(200).render('movies', {detail: true, search:search})
+            } catch (err) {
+                res.status(500).json({ message: err.message });
+            }
+        }
+    }
+})
     },
     getAllMovies: async (req, res) => {
         try {
@@ -94,46 +78,24 @@ const routes = {
         }
     },
     myMovies:async (req, res) =>{
-        try {
-            const data = await Film.find({fav:"true"})
-            res.status(200).json(data);
-        } catch (err) {
-            res.status(500).json({ message: err.message });
-        }
-    },
-    myMovies:async (req, res) =>{
-        try {
-            const data = await Film.find({fav:"true"})
-            res.status(200).json(data);
-        } catch (err) {
-            res.status(500).json({ message: err.message });
-        }
-    },
-    createMovie: async (req, res) => {
-        res.status(200).json({message: 'Se ha guardado'})
-    },
-    editMovie: (req, res) =>{
-        let id = req.params.id
-        res.status(200).json({message: `Se ha actualizado: ${id}`})
-    },
-    deleteMovie: (req, res) =>{
-        res.status(200).json({message: 'Se ha borrado'})
+        if (user) {
+            try {
+                const data = await Film.find({fav:"true"})
+                //res.status(200).json(data);
+                res.status(200).render('movies', {movies: true, data:data})
+            } catch (err) {
+                res.status(500).json({ message: err.message });
+            }
+        } else if (admin) {
+            try {
+                const data = await Film.find();
+                //res.status(200).json(data);
+                res.status(200).render('movies', {movies: true, data:data})
+            } catch (err) {
+                res.status(500).json({ message: err.message });
+            }
+        }  
     }
 }
-
 module.exports = routes;
 
-/* films:async(req, res) => {
-    console.log(req.body.title);
-    let data = await filmData.getfilm(`http://www.omdbapi.com/?t=${req.body.title}&apikey=${apikey}&`)
-    console.log(data);
-    res.status(200).render('film', {data})
-},
-searchFilm:async(req, res) => { //ALEJANDRO
-    let data = await filmData.getfilm(`http://www.omdbapi.com/?t=${req.params.title}&apikey=${apikey}&`)
-    console.log(data);
-    res.status(200).render('film', {data})
-},
-postFilm:async(req, res) => {
-    res.redirect(`/film/${req.body.title2}`)
-}, */
--- routes/movie.js
+++ routes/movie.js
@@ -1,18 +1,11 @@
 const router = require('express').Router(); 
 const movie = require('../controllers/movie');
 
-router.get('/', movie.home)
-// router.get('/signup', movie.getSingUp)
-// router.post('/signup', movie.postSingUp)
-// router.post('/login', movie.login)
-// router.post('/logout', movie.logout)
-router.get('/dashboard', movie.dashboard)
+router.get('/', movie.signIn)
+router.post('/dashboard', movie.dashboard)
 router.get('/search', movie.getMovies)
+router.post('/search', movie.getMovies)
 router.get('/search/:title', movie.searchTitle)
-router.get('/movies', movie.myMovies)
-router.get('/adminTemp', movie.getAllMovies)
-router.post('/createmovie', movie.createMovie)
-router.put('/editmovie/:id', movie.editMovie)
-router.delete('/removemovie', movie.deleteMovie)
+router.post('/movies', movie.myMovies)
 
-module.exports = router
+module.exports = router;
