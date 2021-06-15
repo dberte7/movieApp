7dd148936491650d942ada9216bbf4326181522f
Adrian E. de Zamacona Garza <adriandez@gmail.com>
2021-06-15 09:29:11 +0200

    add event checkbox eventlistener

---
+++ public/boton.js
@@ -0,0 +1,8 @@
+document.getElementById('check').onclick = () => {
+    console.log(document.getElementsByTagName('input')[0].checked);
+}
+
+const valorCheck = (boolean) => {
+    let checked = boolean;
+    return checked
+}
