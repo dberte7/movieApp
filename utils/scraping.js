eda7c0e2100ccc3758c896283cf8448c55ff1719
Diego Bertelloni <diegobertelloni@MacBook-Air.home>
2021-06-19 18:53:56 +0200

    minor changes in scraping

--- utils/scraping.js
+++ utils/scraping.js
@@ -8,17 +8,25 @@ const scrap = async (titulo) => {
     await page.goto(`https://www.sensacine.com/buscar/?q=${titulo}`);
     await page.waitForSelector("#didomi-notice-agree-button")
     await page.click("#didomi-notice-agree-button")
-    await page.waitForSelector('#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.rating-holder.rating-holder-4 > div:nth-child(2) > div > a')
-    await page.click('#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.rating-holder.rating-holder-4 > div:nth-child(2) > div > a');
-    await page.waitForSelector("div.review-card-review-holder > div.content-txt.review-card-content")
+    await page.waitForSelector("#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.meta > h2 > a")
+    await page.click("#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.meta > h2 > a")
     const data = await page.evaluate(() => {
-        const review = document.querySelector("#content-layout > div.titlebar.titlebar-page > div.titlebar-title.titlebar-title-lg > a").innerText;
+        const review = document.querySelector("div.content-txt.review-card-content");
         // console.log(review)
-        return {
-            review
-        }
+        return review.innerText
     }); 
+    console.log(data)
     return data
+    // await page.waitForSelector('#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.rating-holder.rating-holder-4 > div:nth-child(2) > div > a')
+    // await page.click('#content-layout > div.section-wrap.gd-2-cols.gd-gap-30.row-col-sticky > div > section.section.movies-results > ul > li:nth-child(1) > div > div.rating-holder.rating-holder-4 > div:nth-child(2) > div > a');
+    // const data = await page.evaluate(() => {
+    //     const review = document.querySelector("div.titlebar.titlebar-page > div.titlebar-title.titlebar-title-lg > a");
+    //     // console.log(review)
+    //     return {
+    //         review
+    //     }
+    // }); 
+    // return data.innerText
 }
 scrap("hulk")
 
