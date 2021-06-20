f148a8e73c2886265e7091d56ce80f23ce818538
Diego Bertelloni <diegobertelloni@MacBook-Air.home>
2021-06-20 20:12:55 +0200

    css changes

--- controllers/movie.js
+++ controllers/movie.js
@@ -61,7 +61,7 @@ const routes = {
                       let data2 = await movies.getfilm(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
                       search.push(data2);
                     }
-                    res.status(200).render("movies", { searchPage: true, burger: true, search: search });
+                    res.status(200).render("movies", { searchPage: true, title:true, burger: true, search: search });
             } else {
               const dataDb = Film.findOne({Title:titleQ}).lean().exec(async (err, movie) => {
                         let dbSearch = {
@@ -73,7 +73,7 @@ const routes = {
                             Poster: movie.Poster,
                             imdbID: movie._id,
                         }
-                        res.status(200).render("movies", { searchPage: true, burger: true, dbSearch: dbSearch });
+                        res.status(200).render("movies", { searchPage: true, title:true, burger: true, dbSearch: dbSearch });
                       })
             }
         }
@@ -86,11 +86,11 @@ const routes = {
       let data = await movies.getfilm(
         `http://www.omdbapi.com/?i=${id}&apikey=${apikey}&`);
         // scrap(data.Title)
-        res.status(200).render("movies", { detail: true, burger: true, data: data });
+        res.status(200).render("movies", { detail: true, title:true, burger: true, data: data });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
-        },
+  },
     postMovie: (req, res) => {
       const newMovie = req.body
         if (!newMovie.Create) {
@@ -148,7 +148,7 @@ const routes = {
       } else if (rol.admin) {
         try {
           const data = await Film.find();
-          res.status(200).render("admin", { movies: true, data: data, data3: data3 })
+          res.status(200).render("admin", { movies: true,headerGen: true, data: data, data3: data3 })
           data3 = undefined;
         } catch (err) {
           res.status(500).json({ message: err.message });
--- utils/userOrAdmin 2.js
+++
@@ -1,5 +0,0 @@
-const rol = {
-    admin:false
-}
-
-module.exports = rol
